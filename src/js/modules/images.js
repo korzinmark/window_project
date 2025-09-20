f60d9be23ba3914b41ea0bae7fcdf301e295febf
Mark Korzin <korzinmark@Marks-Mac-mini.local>
2025-09-20 14:35:37 +0300

    first commit

---
+++ src/js/modules/images.js
@@ -0,0 +1,32 @@
+const images = () => {
+    const imgPopup = document.createElement('div'),
+          workSection = document.querySelector('.works'),
+          bigImage = document.createElement('img');
+
+    imgPopup.classList.add('popup');
+    workSection.append(imgPopup);
+
+    imgPopup.style.justifyContent = 'center';
+    imgPopup.style.alignItems = 'center';
+    imgPopup.style.display = 'none';
+
+    imgPopup.append(bigImage);
+
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const target = e.target;
+
+        if (target && target.classList.contains('preview')) {
+            imgPopup.style.display = 'flex';
+            const path = target.parentElement.getAttribute('href');
+            bigImage.setAttribute('src', path);
+        }
+
+        if (target && target.matches('div.popup')) {
+            imgPopup.style.display = 'none';
+        }
+    });
+};
+
+export default images;
